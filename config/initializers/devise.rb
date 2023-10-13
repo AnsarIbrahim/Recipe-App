74cce37c93d6c097b21a7c07458fd31a9c668aaf
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-10-13 19:24:07 +0530

    Add Local Host to set render and devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -26,8 +26,7 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
   # config/initializers/devise.rb
-  config.action_mailer.default_url_options = { host: 'recipeapp-85xl.onrender.com' }
-
+ 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
