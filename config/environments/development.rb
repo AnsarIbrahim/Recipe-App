694ac501d7aa80689040d809391f256c47c99a30
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-10-13 19:00:58 +0530

    Add Local Host to set render

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'https://recipeapp-85xl.onrender.com/' }
+  config.action_mailer.default_url_options = { host: 'recipeapp-85xl.onrender.com' }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
