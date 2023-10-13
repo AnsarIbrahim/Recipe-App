96b650e1e6ec1ff26d67984ff8caa28d7f9084b2
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-10-13 19:16:22 +0530

    Add Local Host to set render and devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "recipe_app_production"
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { host: 'recipeapp-85xl.onrender.com' }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -25,6 +25,8 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  # config/initializers/devise.rb
+  config.action_mailer.default_url_options = { host: 'recipeapp-85xl.onrender.com' }
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
