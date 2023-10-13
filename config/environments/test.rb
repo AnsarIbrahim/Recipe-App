acd76c57b33585ba9bd07ddd73f07b8b412f3669
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-10-13 19:25:20 +0530

    Add Local Host to set render and devise

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -62,5 +62,5 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'recipeapp-85xl.onrender.com' }
 end
