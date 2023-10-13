dab6b01aa541f08f5f6b209712d2dba638e6b3d8
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-10-13 18:24:43 +0530

    Change User name again

--- db/migrate/20231013124836_rename_users_to_create_users.rb
+++ db/migrate/20231013124836_rename_users_to_create_users.rb
@@ -1,5 +1,15 @@
 class RenameUsersToCreateUsers < ActiveRecord::Migration[7.1]
-  def change
-    rename_table :users, :create_users
+  def up
+    # Check if the "create_users" table doesn't exist before renaming.
+    unless table_exists?(:create_users)
+      rename_table :users, :create_users
+    end
+  end
+
+  def down
+    # Define how to revert the renaming if necessary.
+    unless table_exists?(:users)
+      rename_table :create_users, :users
+    end
   end
 end
